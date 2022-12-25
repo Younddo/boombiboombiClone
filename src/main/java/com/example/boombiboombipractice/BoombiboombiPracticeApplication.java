5a1cfd03b21f229359894583f78eba37974b8730
Younseo Jang <younseojj@gmail.com>
2022-12-26 01:32:43 +0900

    initial commit

---
+++ src/main/java/com/example/boombiboombipractice/BoombiboombiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.boombiboombipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoombiboombiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoombiboombiPracticeApplication.class, args);
+	}
+
+}
